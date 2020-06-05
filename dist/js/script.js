e99f66ce76df4e26681505da9ba6e2cd49127bfd
nadya <nadya.wyn@ya.ru>
2020-06-05 19:57:53 +0500

    Added JS to Nav in Tablet version

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,4 +21,45 @@ $(document).ready(function () {
 		]
 	});
 
+
+	//Modal
+
+	$('.header__hamburger').on('click', function () {
+		$('.header__nav').fadeIn('slow');
+		$(this).fadeOut('slow');
+	});
+
+	$('.header__nav-close').on('click', function () {
+		$('.header__nav').fadeOut('slow');
+		$('.header__hamburger').fadeIn('slow');
+	});
+
+
+
+	/*
+		$('.button_mini').each(function (i) {
+			$(this).on('click', function () {
+				$('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
+				$('.overlay, #order').fadeIn('slow');
+			});
+		}); */
+
+	//Smooth scroll and pageup
+
+	/* $(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn('slow');
+		}
+		else {
+			$('.pageup').fadeOut('slow');
+		}
+	});
+	
+	$("a[href^='#up']").click(function () {
+		const _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+		return false;
+	});
+	 */
+
 });  
